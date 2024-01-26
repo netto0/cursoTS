c50e7da6bccf05be4b32dee6ca2194ab20d7cf1a
Netto <orlandiomnetto@gmail.com>
2024-01-26 07:39:12 -0300

    1/3 Seção 12 - Projeto ToDo List

---
+++ 12_todo_react/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+//css
+import styles from "./App.module.css";
+
+//components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa"/>
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ 12_todo_react/src/components/TaskForm.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './TaskForm.module.css'
+
+interface Props {
+    btnText: string
+}
+
+const TaskForm = ({btnText}: Props) => {
+  return (
+    <form className={styles.form}>
+        <div className={styles.input_container}>
+            <label htmlFor="title">Título:</label>
+            <input type="text" name='title' placeholder='Título da tarefa'/>
+        </div>
+        <div className={styles.input_container}>
+            <label htmlFor="difficult">Dificuldade:</label>
+            <input type="number" name='difficult' placeholder='Dificuldade da tarefa'/>
+        </div>
+        <input type="submit" value={btnText} />
+    </form>
+  )
+}
+
+export default TaskForm
